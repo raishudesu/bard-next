ab3ddc1e3a9fb270380f7ccba00d24bc2d954e6b
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-10-22 16:19:01 +0800

    added syntax highlighter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
 import { useBard } from "@/stores/useBard";
 import { TConvo } from "@/types/types";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark, atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function Home() {
+  const syntaxHighlighterRef = useRef<SyntaxHighlighter | null>(null);
+
   const [chatHistory, setChatHistory] = useState({});
   const [input, setInput] = useState("");
   const [convo, setConvo] = useState<TConvo[]>([]);
@@ -35,10 +39,29 @@ export default function Home() {
       <div className="text-[#f0f0f0] w-full max-w-screen-lg flex flex-col gap-6">
         {convo.map(({ sender, content }, index) => (
           <Markdown
+            className={`${senderType(sender)} bg-slate-500 p-3`}
             key={index}
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeHighlight]}
-            className={`${senderType(sender)} bg-slate-500 p-6 rounded-lg`}
+            components={{
+              code(props) {
+                const { children, className, node, ...rest } = props;
+                const match = /language-(\w+)/.exec(className || "");
+                return match ? (
+                  <SyntaxHighlighter
+                    {...rest}
+                    style={atomDark}
+                    language={match[1]}
+                    PreTag="div"
+                    ref={(node) => (syntaxHighlighterRef.current = node)}
+                  >
+                    {String(children).replace(/\n$/, "")}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code {...rest} className={className}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
           >
             {content}
           </Markdown>
